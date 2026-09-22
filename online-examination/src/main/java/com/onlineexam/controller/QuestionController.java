7d138d8490ab461d3009580e93b25dd86a9ecc51
Charan707-dotcom <charanaadithya.23mic7127@vitapstudent.ac.in>
2026-09-22 22:05:02 +0530

    Add question management functionality

--- online-examination/src/main/java/com/onlineexam/controller/QuestionController.java
+++ online-examination/src/main/java/com/onlineexam/controller/QuestionController.java
@@ -1,5 +1,40 @@
 package com.onlineexam.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.onlineexam.model.Question;
+import com.onlineexam.repository.QuestionRepository;
+
+@RestController
+@RequestMapping("/questions")
 public class QuestionController {
-    
+
+    private final QuestionRepository questionRepository;
+
+    public QuestionController(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    @GetMapping
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+
+    @PostMapping
+    public Question addQuestion(@RequestBody Question question) {
+        return questionRepository.save(question);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteQuestion(@PathVariable Long id) {
+        questionRepository.deleteById(id);
+    }
 }
